ca1a7d1896ac251420353eac28bf313065c09abe
Ngoc <ngoc.nguyen11@tiki.vn>
2021-04-17 14:42:20 +0700

    Add confirm dialog for book request

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -26,15 +26,26 @@ Page({
     my.navigateTo({url: `pages/mybook/index`});
   },
   onPageSendRequest(product) {
-    app.onAppSendRequest(product);
+    // app.onAppSendRequest(product);
 
+    this.setData({
+      show: true,
+      popup_text_1: 'Bạn có muốn gửi yêu cầu trao đổi sách?',
+      popup_text_2: '(Triết sẽ chọn đổi với 1 quyển trong thư viện của bạn)'
+      })
+  },
+  onCancel() {
+    this.setData({ show: false })    
+  },
+  onSendConfirm() {
     // Show toast
     my.showToast({
       type: 'success',
-      content: 'Sản phẩm đã được thêm vào giỏ hàng',
+      content: 'Đã gửi yêu cầu trao đổi sách',
       buttonText: 'OK',
-      duration: 3000,
+      duration: 2000,
     });
+    this.setData({ show: false })    
   },
   onPageAcceptRequest(product) {
     app.onAppAcceptRequest(product);
