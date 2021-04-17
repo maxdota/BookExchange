6be84d86bf6bee0d4fd4c4bbe226bc9466ce4d8a
Ngoc <ngoc.nguyen11@tiki.vn>
2021-04-17 14:55:57 +0700

    Add dialog for your lib

--- src/pages/your-lib/index.js
+++ src/pages/your-lib/index.js
@@ -24,14 +24,23 @@ Page({
     });
   },
   onPageAcceptRequest(product) {
-    app.onAppSendRequest(product);
-
-    // Show toast
-    my.showToast({
-      type: 'success',
-      content: 'Sản phẩm đã được thêm vào giỏ hàng',
-      buttonText: 'OK',
-      duration: 3000,
-    });
-  }
+    this.setData({
+      show: true,
+      popup_text_1: 'Bạn có muốn chọn sách này?',
+      popup_text_2: '(Triết sẽ nhận sách của bạn để đổi với quyển này)'
+      })
+  },
+  onRequestConfirm() {
+    this.setData({
+      show: false,
+      showFinishPopup: true,
+      popup_text_1: 'Trao đổi sách thành công. Mã giao dịch: 12',
+      popup_text_2: 'Triết sẽ nhận Ông già và biển cả và đưa bạn sách Harry Potter',
+      popup_text_3: 'SĐT của Triết: 0985494839',
+      popup_text_4: 'Vui lòng lưu lại thông tin này bằng screenshot'
+    })  
+  },
+  onCancel() {
+    this.setData({ show: false, showFinishPopup: false })    
+  },
 });
