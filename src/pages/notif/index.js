4f48f892d156e6c5a535643a3513dbcdc75ef7c6
quang.ta@tiki.vn <quang.ta@tiki.vn>
2021-04-17 17:17:17 +0700

    Add notif tab.

---
+++ src/pages/notif/index.js
@@ -0,0 +1,19 @@
+const app = getApp();
+
+Page({
+  data: {
+
+  },
+  onLoad() {
+
+  },
+  onReady() {
+  },
+  onShow() {
+    console.log("notif");
+  },
+  onHide() {
+  },
+  onUnload() {
+  }
+});
